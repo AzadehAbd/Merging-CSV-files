9dd8c42fe07f48d6c28f7fba3e2d3b3619128435
AzadehAbd <107353259+AzadehAbd@users.noreply.github.com>
2022-06-12 15:56:29 +0200

    First Committ

---
+++ Merging CSV files.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 15:44:15 2022
+
+@author: azade
+"""
+
+import geopandas as gpd
+import os
+import rasterio
+import scipy.sparse as sparse 
+import pandas as pd
+import numpy as np
+
+
+master_df=pd.DataFrame()
+for csvfiles in os.listdir(os.getcwd()):
+    if csvfiles.endswith('.csv'):
+        master_df=master_df.append(pd.read_csv(csvfiles))
+        
+master_df.to_csv("E:\Digital data\Andreas\masterfile2.csv", index=False)
